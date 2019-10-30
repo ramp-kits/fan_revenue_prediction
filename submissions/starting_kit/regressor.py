58cc9338cce5fd4bd7c6b6dcac8ecd7122d8bdbc
Lucy Liu <jliu176@gmail.com>
2019-10-30 18:17:26 +0100

    reduce n_estimators to reduce running time

--- submissions/starting_kit/regressor.py
+++ submissions/starting_kit/regressor.py
@@ -5,7 +5,7 @@ from sklearn.base import BaseEstimator
 class Regressor(BaseEstimator):
     def __init__(self):
         self.reg = RandomForestRegressor(
-            n_estimators=20, max_depth=50, max_features=10)
+            n_estimators=10, max_depth=50, max_features=10)
 
     def fit(self, X, y):
         self.reg.fit(X, y)
